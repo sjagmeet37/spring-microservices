88f12ad2a81109023d2fe6f9b47d0b7d5fbc4425
Jagmeet Sandhu <sjagmeet37@gmail.com>
2024-03-23 21:17:15 +0530

    prepopulating data

--- inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
@@ -1,5 +1,8 @@
 package com.microservice.inventoryservice;
 
+import com.microservice.inventoryservice.model.Inventory;
+import com.microservice.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,23 @@ public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(10);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("iphone_13");
+			inventory2.setQuantity(10);
+
+			inventoryRepository.save(inventory2);
+			inventoryRepository.save(inventory);
+		};
+
 	}
 
 }
